0cf23b12b421bba8d786a6d3b2d92181ff6b8273
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-05-05 20:06:22 +0500

    server changes

--- Clinic-management-system/backend/api/server.js
+++ Clinic-management-system/backend/api/server.js
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB();
 
-// const server = app.listen(PORT, () => {
-//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-// });
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
 
-// // Handle unhandled promise rejections
-// process.on('unhandledRejection', (err, promise) => {
-//   console.log(`Error: ${err.message}`);
-//   // Close server & exit process
-//   server.close(() => process.exit(1));
-// });
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
