08edf1f4ae55377515a6517d1d44b44abce3ef5e
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-05-05 19:07:28 +0500

    server is commit

--- Clinic-management-system/backend/api/server.js
+++ Clinic-management-system/backend/api/server.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB();
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+// const server = app.listen(PORT, () => {
+//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+// });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
