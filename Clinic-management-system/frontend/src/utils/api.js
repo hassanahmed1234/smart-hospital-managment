f6e116eaaf8e4baca6c66fa3e1af536e36e96e5b
Hassan Ahmed <your-email@example.comhafizhassanahmedqadri123@gmail.com>
2026-05-07 10:40:01 +0500

    frontend url

--- Clinic-management-system/frontend/src/utils/api.js
+++ Clinic-management-system/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://smart-hospital-managment.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://smart-hospital-managment.vercel.app/',
 });
 
 // Add a request interceptor to add the auth token to headers
